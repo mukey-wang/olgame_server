d960688db344eaccb6b72540f13830e96d10ebee
mukey-wang <56380763+mukey-wang@users.noreply.github.com>
2024-03-29 12:23:03 +0800

    Add files via upload

---
+++ BuildConfig.java
@@ -0,0 +1,8 @@
+
+package emu.lunarcore;
+
+public final class BuildConfig {
+	public static final String VERSION = "1.2.0";
+    public static final String GIT_HASH = "";
+    public static final String GIT_TIMESTAMP = "";
+}
